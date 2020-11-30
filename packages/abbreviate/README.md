06398f7dd7f255f3a7785d7f8682c166140a6cb3
Austin Paquette <austin.paquette@forcir.com>
2020-11-29 23:21:35 -0800

    add @pqt/abbreviate library

---
+++ packages/abbreviate/README.md
@@ -0,0 +1,7 @@
+# abbreviate
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test abbreviate` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/abbreviate/src/lib/abbreviate.ts
@@ -0,0 +1,23 @@
+export function abbreviate(number: number, decimalPlaces = 0): string {
+  let letter = '';
+  decimalPlaces = Math.pow(10, decimalPlaces);
+  const abbrev = ['K', 'M', 'B', 'T'];
+
+  for (let i = abbrev.length - 1; i >= 0; i--) {
+    const size = Math.pow(10, (i + 1) * 3);
+
+    if (size <= number) {
+      number = Math.round((number * decimalPlaces) / size) / decimalPlaces;
+
+      if (number === 1000 && i < abbrev.length - 1) {
+        number = 1;
+        i++;
+      }
+
+      letter = abbrev[i];
+      break;
+    }
+  }
+
+  return number.toString() + letter;
+}
